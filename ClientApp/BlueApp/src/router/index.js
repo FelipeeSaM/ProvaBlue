68007f9be180d04d5d5ac4b4d852f1d823f606f9
eu mesmo <luiz_sena@hotmail.com>
2024-04-15 16:17:05 -0300

    Configuring files

--- ClientApp/BlueApp/src/router/index.js
+++ ClientApp/BlueApp/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: LoginView
     },
     {
       path: '/about',
--- ClientApp/BlueApp/src/views/AboutView.vue
+++ ClientApp/BlueApp/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page!</h1>
   </div>
 </template>
 
@@ -9,7 +9,7 @@
   .about {
     min-height: 100vh;
     display: flex;
-    align-items: center; 
+    align-items: center;
   }
 }
 </style>
